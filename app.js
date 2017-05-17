5b480d4dab66f221dfb4d2873e5c46d3a9273e1d
Devin G <engy100x@gmail.com>
2017-05-16 19:57:52 -0700

    working on constructor

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 'use strict'
+
+var projects = [];
+
+function Project(rawDataObj){
+  this.title = rawDataObj.title;
+  this.collaborators = rawDataObj.collaborators;
+  this.dateCompleted = rawDataObj.dateCompleted;
+  this.body = rawDataObj.body;
+}
