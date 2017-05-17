90316e3533f4c083344e6b573b8965ea3c0bccc1
Devin G <engy100x@gmail.com>
2017-05-17 09:03:27 -0700

    added in element to contain profile pic, added few more parameters for constructor

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var projects = [];
 
 function Project(rawDataObj){
   this.title = rawDataObj.title;
+  this.projectUrl = rawDataObj.projectUrl;
+  this.projectImg = rawDataObj.projectImg;
   this.collaborators = rawDataObj.collaborators;
   this.dateCompleted = rawDataObj.dateCompleted;
   this.body = rawDataObj.body;
