a082f9b36206cdb75a372b617be3132674e17db8
Devin G <engy100x@gmail.com>
2017-06-05 14:27:39 -0700

    wrapped in IIFE, fixed non-persistence when page reload

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,43 +1,55 @@
 'use strict'
 
+var app = app || {};
 
-function Project(rawDataObj){
-  this.title = rawDataObj.title;
-  this.projectUrl = rawDataObj.projectUrl;
-  this.projectImg = rawDataObj.projectImg;
-  this.collaborators = rawDataObj.collaborators;
-  this.dateCompleted = rawDataObj.dateCompleted;
-  this.body = rawDataObj.body;
-}
+(function(module) {
 
-Project.all = [];
-
-Project.prototype.toHtml = function() {
-  const projectRender = Handlebars.compile($('.projects-template').html());
-  return projectRender(this);
-}
-
-Project.loadAll = function(rawData){
-  rawData.forEach(function(ele){
-    Project.all.push(new Project (ele));
-  })
-}
-
-
-Project.fetchAll = function(){
-  if (localStorage.projects) {
-    Project.loadAll(JSON.parse(localStorage.projects));
-  } else {
-    $.getJSON('data/projectData.json').then(function(rawData){
-      localStorage.projects = JSON.stringify(rawData);
-      Project.loadAll(rawData);
-      Project.all.forEach(function(project){
-        $('#projects').append(project.toHtml());
-      });
-    },
-    function(err){
-      console.log(err);
-    }
-  )
+  function Project(rawDataObj){
+    this.title = rawDataObj.title;
+    this.projectUrl = rawDataObj.projectUrl;
+    this.projectImg = rawDataObj.projectImg;
+    this.collaborators = rawDataObj.collaborators;
+    this.dateCompleted = rawDataObj.dateCompleted;
+    this.body = rawDataObj.body;
   }
-}
+
+  Project.all = [];
+
+  Project.prototype.toHtml = function() {
+    const projectRender = Handlebars.compile($('.projects-template').html());
+    return projectRender(this);
+  }
+
+  Project.loadAll = function(rawData){
+    rawData.forEach(function(ele){
+      Project.all.push(new Project (ele));
+    })
+  }
+
+  Project.fetchAll = function(){
+    if (localStorage.projects) {
+      Project.loadAll(JSON.parse(localStorage.projects));
+      Project.initIndex();
+    } else {
+      $.getJSON('data/projectData.json').then(function(rawData){
+        localStorage.projects = JSON.stringify(rawData);
+        Project.loadAll(rawData);
+        Project.all.forEach(function(project){
+          $('#projects').append(project.toHtml());
+        });
+      },
+      function(err){
+        console.log(err);
+      }
+    )
+    }
+  }
+
+  Project.initIndex = function() {
+    Project.all.forEach(function(project){
+      $('#projects').append(project.toHtml())
+    });
+  }
+
+  module.Project = Project;
+})(app);
